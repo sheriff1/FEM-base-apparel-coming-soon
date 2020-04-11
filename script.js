d34b787a8c3bd60ece0ded0ea4a6894cbf29f32c
Sheriff <sheriff.jolaoso@gmail.com>
2020-04-11 11:41:46 -0700

    add additional polish

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById("button").addEventListener('click', () => {
+    document.getElementById("button").setAttribute("style", "opacity: 0.5");
+    document.getElementById("button").setAttribute("style", "z-index: 2");
     document.getElementById("bigform").setAttribute("style", "border: 2px solid hsl(0, 93%, 68%);");
     document.getElementsByClassName("error-message")[0].style.display = 'block';
-
+    document.getElementsByClassName("error-icon")[0].style.display = 'block';
 });
 
 document.getElementById("bigform").setCustomValidity(" ");
