ef6e8fc7ee8d86740d5b96630cc528cce69844a5
Sheriff <sheriff.jolaoso@gmail.com>
2020-04-11 13:31:50 -0700

    works but not on firefox :/

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.getElementById("button").addEventListener('click', () => {
-    document.getElementById("button").setAttribute("style", "opacity: 0.5");
-    document.getElementById("button").setAttribute("style", "z-index: 2");
-    document.getElementById("bigform").setAttribute("style", "border: 2px solid hsl(0, 93%, 68%);");
-    document.getElementsByClassName("error-message")[0].style.display = 'block';
-    document.getElementsByClassName("error-icon")[0].style.display = 'block';
+    if (document.getElementById('bigform').validity.typeMismatch) {
+        document.getElementById("button").setAttribute("style", "opacity: 0.5");
+        document.getElementById("button").setAttribute("style", "z-index: 2");
+        document.getElementById("button").setAttribute("style", "background: linear-gradient(135deg, hsl(0, 90%, 96%), hsl(0, 84%, 84%))");
+        document.getElementById("bigform").setAttribute("style", "border: 2px solid hsl(0, 93%, 68%);");
+        document.getElementsByClassName("error-message")[0].style.display = 'block';
+        document.getElementsByClassName("error-icon")[0].style.display = 'block';
+    } else {
+        document.getElementById("button").setAttribute("style", "opacity: 1");
+        document.getElementById("button").setAttribute("style", "z-index: 2");
+        document.getElementById("button").setAttribute("style", "background: linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))");
+        document.getElementById("bigform").setAttribute("style", "border-color: hsl(0, 36%, 70%);");
+        document.getElementsByClassName("error-message")[0].style.display = 'none';
+        document.getElementsByClassName("error-icon")[0].style.display = 'none';
+    }
 });
 
 document.getElementById("bigform").setCustomValidity(" ");
